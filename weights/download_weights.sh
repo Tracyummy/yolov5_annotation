4d23129304479b82fada2ef68236a33d55b89224
zhaichao <18742518667@163.com>
2021-07-05 15:46:12 +0800

    add folder

---
+++ weights/download_weights.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Download common models
+
+python3 -c "
+import sys;
+sys.path.append('../');
+from utils.google_utils import *;
+attempt_download('weights/yolov5s.pt');
+attempt_download('weights/yolov5m.pt');
+attempt_download('weights/yolov5l.pt');
+attempt_download('weights/yolov5x.pt')
+"
